bcad277948166fcf0890c490197e002e5ea66931
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-12-26 21:59:02 +0530

    Amount created

--- sm.py
+++ sm.py
@@ -1,4 +1,13 @@
 
 def deposit():
     while True:
-        
+        amount = input("What would you like to deposit? $ : ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please enter an amount greater than zero.")
+        else:
+            print("Invalid input. Please enter a numeric value.")
+    return amount
