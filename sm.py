e9c7b5076913993846f39cd22c9553bfb9d6239e
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-12-26 22:17:35 +0530

    bets added

--- sm.py
+++ sm.py
@@ -1,5 +1,7 @@
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -27,10 +29,24 @@ def get_no_of_lines():
             print("Invalid input. Please enter a numeric value.")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $ : ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print("Amount must be between ${MIN_BET} and ${MAX_BET}.")
+        else:
+            print("Invalid input. Please enter a numeric value.")
+    return amount
+
 
 def main():
     balance = deposit()
     lines = get_no_of_lines() 
-    print(balance, lines)
+    bet = get_bet()
+    print(balance, lines, bet)
 
 main()
